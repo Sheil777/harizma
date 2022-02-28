af8450d464f60f9f74a24d5ac8a199a6937674d6
Sheil <karaulov.z@mail.ru>
2022-02-28 21:38:45 +0300

    Bug fixed with scorll in adaptive phone menu

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(
                 $('div.header__burger').toggleClass('header__burger_close');
                 $('header.header').toggleClass('header_phone');
                 $('div.header__city_phone').toggleClass('header__city_phoneDisable');
+                $('body').toggleClass('lock');
             }
         );
     }
