e5cd9dc51747b9cd1931a95ae4e39ea78d33d37f
SheilAPK <karaulov.z@mail.ru>
2022-03-03 17:16:03 +0300

    In work

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,5 +1,10 @@
+function editNameCity(name) {
+    $('span.main__textCity')[0].textContent = name;
+    $('div.header__cityName')[0].textContent = name;
+}
+
 $(document).ready(
-    function(){
+    function() {
         $('.header__burger').click(
             function (event) {
                 $('div.header__burger').toggleClass('header__burger_close');
@@ -7,6 +12,29 @@ $(document).ready(
                 $('div.header__city_phone').toggleClass('header__city_phoneDisable');
                 $('div.header__block, .header__logo').toggleClass('active');
                 $('body').toggleClass('lock');
+            }
+        );
+
+        $('.header__cityName, .header__circle, .header-city-list__item').click(
+            function (event) {
+                $('.header-city-list').toggleClass('active');
+            }
+        );
+
+        // Если куки не записаны
+        if($.cookie('city') == null) {
+            $.cookie('city', 'м.Лубянка');
+        }
+
+        $('.header-city-list').click(
+            function (event) {
+                var el = event.target;
+                var city = el.textContent;
+                el.style.color = '#F69020';
+
+                $.cookie('city', city);
+
+                editNameCity(city);
             }
         );
     }
