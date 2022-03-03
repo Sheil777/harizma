c0808d2a710dc0481b9446dd1481d0c37e551aef
SheilAPK <karaulov.z@mail.ru>
2022-03-03 13:35:44 +0300

    Update header

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(
                 $('div.header__burger').toggleClass('header__burger_close');
                 $('header.header').toggleClass('header_phone');
                 $('div.header__city_phone').toggleClass('header__city_phoneDisable');
+                $('div.header__block, .header__logo').toggleClass('active');
                 $('body').toggleClass('lock');
             }
         );
