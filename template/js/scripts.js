1d8d781edc5a818bb0815d3ebba483d08276d1b7
SheilNoteBook <karaulov.z@mail.ru>
2022-03-03 17:55:51 +0300

    Dropdown menu in work

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -3,6 +3,31 @@ function editNameCity(name) {
     $('div.header__cityName')[0].textContent = name;
 }
 
+function editColorCities(el) {
+    //el.parentElement;
+
+    $('.header-city-list__item').each(
+        function (index, element) {
+            element.style.color = '#FFFFFF';
+        }
+    );
+
+    el.style.color = '#F69020';
+}
+
+function editColorCityByName(name) {
+    $('.header-city-list__item').each(
+        function (index, element) {
+            if(element.textContent == name)
+                element.style.color = '#F69020';
+            else
+                element.style.color = '#FFFFFF';
+        }
+    );
+
+    //el.style.color = '#F69020';
+}
+
 $(document).ready(
     function() {
         $('.header__burger').click(
@@ -26,15 +51,18 @@ $(document).ready(
             $.cookie('city', 'м.Лубянка');
         }
 
+        editNameCity($.cookie('city'));
+        editColorCityByName($.cookie('city'));
+
         $('.header-city-list').click(
             function (event) {
                 var el = event.target;
                 var city = el.textContent;
-                el.style.color = '#F69020';
 
                 $.cookie('city', city);
 
                 editNameCity(city);
+                editColorCities(el);
             }
         );
     }
